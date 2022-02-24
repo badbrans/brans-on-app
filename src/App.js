94035d1bfb7719e7e47784d9d205dc2ed9751715
badbrans <raphael_brans@hotmail.com>
2022-02-24 13:52:14 -0300

    Primeiro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+  const [dias, setDias] = useState('');
+  const [horas, setHoras] = useState('');
+
+  const [mensagem, setMensagem] = useState('');
+  
+function calcularHoras(){
+  const totalDias = dias * 8;
+  const media = horas - totalDias  ;
+
+  if(media === 0){
+  setMensagem("Você não deve e não tem horas sobrando.Total de horas: " + media.toFixed(0))
+}else if(media > 0){
+  setMensagem("Você tem horas sobrando! Total de horas: " + media.toFixed(0))
+}else if(media < 0){
+  setMensagem("Você está devendo horas! Total de horas: " + media.toFixed(0))
+
 }
 
-export default App;
+}
+  return(
+    <div className="app">
+      <h1>Cálculo de banco de horas</h1>
+      <span> Calcular total de horas</span>
+
+      <div className="area-input"> 
+      <input
+      type="text"
+      placeholder="Total de dias trabalhados. Ex:60"
+      value={dias}
+      onChange={ (e) => setDias(e.target.value)}
+      />
+      
+      <input
+      type="text"
+      placeholder="Total de horas trabalhadas. Ex: 200"      
+      value={horas}
+      onChange={ (e) => setHoras(e.target.value)}     
+      />
+    <button onClick={calcularHoras}>
+      Calcular
+      </button>  
+    </div>
+    <h2>{mensagem}</h2>
+    </div>
+  )
+}
